612a95ec32a891d4479210d450cb35ac3139d225
Kevin Guo <kg344@cornell.edu>
2019-07-23 12:51:10 -0400

    split into chunks

--- gh_binary_convert.py
+++ gh_binary_convert.py
@@ -4,19 +4,45 @@
     Output:
         a: The a output variable"""
 
-__author__ = "user"
+__author__ = "Kevin Guo"
 __version__ = "2019.07.23"
 
 BITS = 16
-def interpret(number):
-	output = [None] * BITS
+def interpretChunks(number):
+	# sets up default values of variables
+	num_chunks = 1
 	is_negative = False
 
-	if number < 0:
-		is_negative = True
-		number = abs(number)
+	chunks = []
+	output = []
+	num_chunks = []
 
-	binary = bin(number)[2:].zfill(BITS-1)
+	# splits the number into chunks
+	if number > 32767:
+		num_chunks = int(number / 32767)
+		for i = range(num_chunks):
+			chunks.append(32767)
+		if number % 32767 > 0:
+			chunks.append(number % 32767)
+			num_chunks += 1
+	elif number < -32768:
+		num_chunks = int(number / -32768)
+		for i = range(num_chunks):
+			chunks.append(-32768)
+		if number % -32768 < 0:
+			chunks.append(number % -32768)
+			num_chunks += 1
+
+	for i in range(num_chunks):
+		n = interpret()
+	return output
+
+def interpret(num):
+	if num < 0:
+		is_negative = True
+		num = abs(num)
+
+	binary = bin(num)[2:].zfill(BITS-1)
 
 	for i in range(BITS-2,-1,-1):
 		if int(binary[i]) == 1:
