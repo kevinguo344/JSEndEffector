71a13fb4eaa199f94afbc822453abc87213a20fc
Kevin Guo <kg344@cornell.edu>
2019-08-06 17:42:36 -0400

    attempt 2 at fixing debouncing issue with machina binary

--- machina_input_test/machina_input_test.ino
+++ machina_input_test/machina_input_test.ino
@@ -5,7 +5,7 @@ const int digital_pins[] = {22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52};
 const int bit_values[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,-1};
 
 int num_steps = 0, pulse_width = 0, prev_input = 0;
-int signal_change = 0;
+int change_time = 0;
 
 void setup(){
   Serial.begin(115200);
@@ -16,14 +16,11 @@ void setup(){
 void loop(){
   // SEQUENCE: NUMBER OF STEPS, -1, PULSE_WIDTH
   int curr_input = readBinary();
-  if (curr_input != prev_input){
-    signal_change = millis();
-  }
-  if((millis() - signal_change) > MIN_DELAY){
+  if (curr_input != prev_input){ change_time = millis(); }
+  if (millis() - change_time > MIN_DELAY && curr_input != prev_input && curr_input == readBinary()){
     Serial.println(curr_input);
     prev_input = curr_input;
   }
-  //delay(100);
 }
 
 int readBinary(){
