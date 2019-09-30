23716d0037ace7118c9ea71e7970a644696a6895
Kevin Guo <kg344@cornell.edu>
2019-09-30 15:49:28 -0400

    added sensing to stepper control

--- stepper_control_digital_simple/stepper_control_digital_simple.ino
+++ stepper_control_digital_simple/stepper_control_digital_simple.ino
@@ -1,28 +1,51 @@
+// Output Pin definitions
 #define STEP_PIN 9
 #define DIRECTION_PIN 10
+#define ENDSTOP_BACK_PIN 11
 
+// Input Pin definitions
 #define ON_OFF_PIN 22
 #define DIR_INPUT_PIN 24
 
-#define PULSE_WIDTH 25
+// Distance Sensor constants
+#include <Wire.h>
+#include <VL6180X.h>
+VL6180X sensor;
 
-bool curr_on_off;
-bool curr_dir;
+// Endstop Switch constants
+#define SWITCH_ON 0
+#define SWITCH_OFF 1
+
+// Pulse Width constants
+#define PULSE_WIDTH 25
+#define RETRACT_PULSE_WIDTH 50
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+
+  // sets up mode for digital pins
   pinMode(STEP_PIN, OUTPUT); pinMode(DIRECTION_PIN, OUTPUT); 
-  pinMode(ON_OFF_PIN, INPUT); pinMode(DIR_INPUT_PIN, INPUT);
+  pinMode(ON_OFF_PIN, INPUT); pinMode(DIR_INPUT_PIN, INPUT); pinMode(ENDSTOP_BACK_PIN, INPUT);
+
+  // sets up distance sensor
+  Wire.begin();
+  sensor.init(); sensor.configureDefault();
+  sensor.setScaling(2); sensor.setTimeout(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(digitalRead(ON_OFF_PIN)){
     Serial.println("Stepper On");
-    if(digitalRead(DIR_INPUT_PIN)){ Serial.println("Forward"); }
-    else{ Serial.println("Backwards"); }
-    step(digitalRead(DIR_INPUT_PIN), PULSE_WIDTH);
+    if(digitalRead(DIR_INPUT_PIN)){
+      // if the movement is forward, check the distance sensor
+      if (sensor.readRangeSingleMillimeters() < 500){ step(digitalRead(DIR_INPUT_PIN), PULSE_WIDTH); }
+      else{ Serial.println("DISTANCE SENSOR ACTIVATED"); }
+    }
+    else{
+      // if the movement is backwards, check the endstop sensor
+      if(digitalRead(ENDSTOP_BACK_PIN) == SWITCH_OFF){ step(digitalRead(DIR_INPUT_PIN), PULSE_WIDTH); }
+      else{ Serial.println("ENDSTOP ACTIVATED"); }
+    }
   }
 }
 
@@ -34,4 +57,10 @@ void step(bool forward, float delayMs){
   delayMicroseconds(delayMs);                                       // delay must be greater than 3 us according to Arduino docs
   digitalWrite(STEP_PIN, LOW);                                      // finishes pulse by going to low
   delayMicroseconds(100);
+}
+
+void retractPosition(){
+  Serial.println("Retract Extruder");
+  while(digitalRead(ENDSTOP_BACK_PIN) == SWITCH_OFF){ step(false, RETRACT_PULSE_WIDTH); }
+  Serial.println("Extruder Fully Retracted");
 }
